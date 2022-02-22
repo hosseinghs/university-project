1cc59f1a51696c9260c5e91b3f48afbc1725ff9c
Hossein Samani <samanihossein1379@gmail.com>
2022-02-22 23:32:40 +0330

    server deleted

--- server/routes/articles.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/getArticles", async (req, res) => {
-  return res.status(200).send("okaye");
-});
-
-module.exports = router;
