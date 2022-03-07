7480e60329c4312264d87882fd495caa4cae1973
Hossein Samani <samanihossein1379@gmail.com>
2022-03-07 19:26:07 +0330

    codes for mongoose deleted

--- server/models/chetor/users.js
+++ server/models/chetor/users.js
@@ -1,55 +1,54 @@
-const jwt = require("jsonwebtoken");
-const mongoose = require("mongoose");
+// const jwt = require("jsonwebtoken");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => v && v.length > 0,
-      message: "اسم حداقل باید 3 حرف باشد!",
-    },
-  },
-  lastName: {
-    type: String,
-    required: true,
-  },
-  phoneNumber: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => v && v.length === 11,
-      message: "شماره ی تماس باید 11 رقمی باشد!",
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    validate: {
-      validator: (v) => v && v.length >= 6,
-      message: "رمزعبور باید حداقل 6 کاراکتر باشد!",
-    },
-  },
-});
+// const userSchema = new mongoose.Schema({
+//   firstName: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: (v) => v && v.length > 0,
+//       message: "اسم حداقل باید 3 حرف باشد!",
+//     },
+//   },
+//   lastName: {
+//     type: String,
+//     required: true,
+//   },
+//   phoneNumber: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: (v) => v && v.length === 11,
+//       message: "شماره ی تماس باید 11 رقمی باشد!",
+//     },
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: (v) => v && v.length >= 6,
+//       message: "رمزعبور باید حداقل 6 کاراکتر باشد!",
+//     },
+//   },
+// });
 
-function generateToken(user) {
-  const payload = {
-    id: user._id,
-    phoneNumber: user.phoneNumber,
-    password: user.password,
-  };
-  const token = jwt.sign(payload, "secret", {
-    noTimestamp: false,
-    expiresIn: "1h",
-  });
-  return token;
-}
-const User = mongoose.model("User", userSchema);
+// function generateToken(user) {
+//   const payload = {
+//     id: user._id,
+//     phoneNumber: user.phoneNumber,
+//     password: user.password,
+//   };
+//   const token = jwt.sign(payload, "secret", {
+//     noTimestamp: false,
+//     expiresIn: "1h",
+//   });
+//   return token;
+// }
+// const User = mongoose.model("User", userSchema);
 
-function validateUser(user) {
-  return User(user);
-}
+// function validateUser(user) {
+//   return User(user);
+// }
 
-exports.validateUser = validateUser;
-exports.User = User;
-exports.generateToken = generateToken;
+// exports.validateUser = validateUser;
+// exports.User = User;
+// exports.generateToken = generateToken;
--- server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/getArticels", (req, res) => {
-  return res.status(200).send("ok");
-});
-
-module.exports = router;
