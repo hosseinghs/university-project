1a06f2cffaecc78586fbc83407d3eb5f5f4bdaca
Hossein Samani <samanihossein1379@gmail.com>
2022-03-08 23:18:39 +0330

    baseUrl added

--- admin/nuxt.config.js
+++ admin/nuxt.config.js
@@ -20,7 +20,7 @@ export default {
 
   css: ['~/assets/main.scss'],
   env: {
-    baseUrl: 'http://localhost:1000/',
+    baseUrl: 'http://localhost:1000/admin',
   },
   plugins: ['~/plugins/apiCaller'],
 
