cd285fe26bd15c98d6b0a560953b429c4aeba256
Hossein Samani <samanihossein1379@gmail.com>
2022-03-08 23:18:28 +0330

    general functions added

--- admin/utils/general.js
+++ admin/utils/general.js
@@ -1,6 +1,34 @@
+export function addToArr(arr, v) {
+  if (Array.isArray(v)) arr.push(...v)
+  else arr.push(v)
+}
+
+export function faMoney(data) {
+  return (data / 10)
+    .toString()
+    .replace(/\D/g, '')
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
+}
+
+export function removeDuplicatesFromArr(arr) {
+  return [...new Set(arr)]
+}
+
 export function createObjWithDefaultValues(self, defaults, options) {
-    const res = Object.assign({}, defaults, options)
-    Object.keys(defaults).forEach((k) => {
-      self[k] = res[k]
-    })
-  }
+  const res = Object.assign({}, defaults, options)
+  Object.keys(defaults).forEach((k) => {
+    self[k] = res[k]
+  })
+}
+
+export function deleteKeyFromObj(obj, ...keys) {
+  keys.forEach((k) => {
+    delete obj[k]
+  })
+}
+
+export function faDate(date) {
+  if (date && date !== '0001-01-01T00:00:00')
+    return new Date(date).toLocaleDateString('fa-IR')
+  return null
+}
