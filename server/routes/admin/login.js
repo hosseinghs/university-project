b1c0ef9c5668518762972b0caeb40360cfff7d0e
Hossein Samani <samanihossein1379@gmail.com>
2022-03-09 00:05:24 +0330

    login api improved

--- server/routes/admin/login.js
+++ server/routes/admin/login.js
@@ -4,9 +4,16 @@ const sql = require("mssql");
 
 router.post("/", async (req, res) => {
   const { phoneNumber, password } = req.body;
+  if (!phoneNumber || !password)
+    return res
+      .status(400)
+      .send({ success: false, des: "رمزعبور و یا شماره تلفن اشتباه است!" });
   console.log(phoneNumber, password);
-  //   const categoryList = await sql.query("SELECT * FROM category");
-  return res.status(200).send({ success: true, res: "" });
+  const user = await sql.query(
+    `SELECT * FROM admin where phoneNumber=${phoneNumber}`
+  );
+  console.log(user.recordsets);
+  return res.status(200).send({ success: true, res: user });
 });
 
 module.exports = router;
