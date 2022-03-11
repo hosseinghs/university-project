d4063a08a8f8b3d37901a2bc29c14c51f4b2243a
Hossein Samani <samanihossein1379@gmail.com>
2022-03-11 13:10:05 +0330

    prime btn added

---
+++ admin/components/form/Btn/Prime.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn color="#485ec4" v-bind="$attrs" v-on="$listeners">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'LoginBtnComponent',
+}
+</script>
