9da771ea3fa2ceed0b9663f7d57b7cf1b5559693
Hossein Samani <samanihossein1379@gmail.com>
2022-03-11 13:09:59 +0330

    cancel btn added

---
+++ admin/components/form/Btn/Cancel.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn v-bind="$attrs" color="#FF5252" v-on="$listeners">
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'CancelBtnComponent',
+}
+</script>
