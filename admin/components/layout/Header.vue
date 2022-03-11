143e63bcff0d83f2a42001568d37ed73021d6df2
Hossein Samani <samanihossein1379@gmail.com>
2022-03-11 13:10:23 +0330

    header component and logout method added

---
+++ admin/components/layout/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-app-bar elevation="0" app>
+    <FormBtnCancel outlined @click.stop="logout()"> خروج </FormBtnCancel>
+  </v-app-bar>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+export default {
+  name: 'HeaderComponent',
+  methods: {
+    ...mapActions('login', ['setLoggedInState']),
+    logout() {
+      this.setLoggedInState(false)
+      localStorage.removeItem('token')
+      this.$router.push({ path: '/' })
+    },
+  },
+}
+</script>
