db2f61c45bf2ea6ba7149a1a5c2094187265c855
Hossein Samani <samanihossein1379@gmail.com>
2022-03-11 12:50:12 +0330

    login ui improved

--- admin/layouts/login.vue
+++ admin/layouts/login.vue
@@ -1,7 +1,9 @@
 <template>
   <v-app style="background-color: #f5f5f5">
     <LayoutNotification />
-    <v-main>
+    <v-main
+      style="width: 100%; heigth: 100vh; display: grid; place-items: center"
+    >
       <nuxt />
     </v-main>
   </v-app>
