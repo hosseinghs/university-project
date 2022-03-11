27c76f1950f5d8f3c83b4eca67eb324275b4cd63
Hossein Samani <samanihossein1379@gmail.com>
2022-03-11 17:56:40 +0330

    getArticle api added

--- server/routes/admin/articles.js
+++ server/routes/admin/articles.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const sql = require("mssql");
 const checkToken = require("../../middleware/auth");
 
-router.get("/", checkToken, async (_, res) => {
+router.get("/category", checkToken, async (_, res) => {
   const sqlRes = await sql.query("SELECT * FROM category");
   const categoryList = sqlRes.recordsets[0];
   res.status(200).send({ success: true, res: categoryList });
@@ -11,12 +11,16 @@ router.get("/", checkToken, async (_, res) => {
 
 router.post("/create", checkToken, async (req, res) => {
   const { title, categoryId, createdDate, author, text } = req.body;
-
-  sql.query`INSERT INTO article ( title , categoryId , author , date , text ) VALUES (${title},${categoryId},${author},${createdDate},${text})`,
+  await sql.query`INSERT INTO article ( title , categoryId , author , date , text ) VALUES (${title},${categoryId},${author},${createdDate},${text})`,
     (err, res) => {
       if (err) res.status(400).send({ success: false, des: err });
       return res.send({ success: true, des: "با موفقیت افزوده شد!" });
     };
 });
 
+router.get("/get", checkToken, async (_, res) => {
+  const sqlRes = await sql.query`SELECT * FROM article`;
+  const articlesList = sqlRes.recordsets[0];
+  res.status(200).send({ success: true, res: articlesList });
+});
 module.exports = router;
