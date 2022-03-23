ebf3eec164c36e2553d85206fa1d24a083abd58d
Hossein Samani <samanihossein1379@gmail.com>
2022-03-23 20:25:13 +0430

    tooptip component added

---
+++ admin/components/Ui/Tooltip.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-tooltip bottom>
+    <template #activator="{ on, attrs }">
+      <div style="display: inline-block" v-bind="attrs" v-on="on">
+        <slot></slot>
+      </div>
+    </template>
+    <span>{{ text }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+export default {
+  name: 'TooltipComponent',
+  props: {
+    text: {
+      type: String,
+      default: 'tooltip',
+      require: true,
+    },
+  },
+}
+</script>
