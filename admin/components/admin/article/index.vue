d3a4324a21ef8d64dfbc05781d6da11c7ec18cdf
Hossein Samani <samanihossein1379@gmail.com>
2022-03-23 20:36:30 +0430

    warning added

--- admin/components/admin/article/index.vue
+++ admin/components/admin/article/index.vue
@@ -4,10 +4,38 @@
     <div v-html="item.text"></div>
     <div>نوشته شده توسط {{ item.author }}</div>
     <div>تاریخ ایجاد {{ faDate(item.date) }}</div>
+    <v-card-actions>
+      <UiTooltip v-if="isPublished" text="نامنتشر کردن">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          small
+          :color="isPublished ? 'red' : 'primary'"
+          @click.stop="generateWarningConfig(item)"
+        >
+          <v-icon dark>mdi-upload-off</v-icon>
+        </v-btn>
+      </UiTooltip>
+      <UiTooltip v-else text="انتشار">
+        <v-btn
+          class="mx-2"
+          fab
+          dark
+          small
+          :color="isPublished ? 'red' : 'primary'"
+          @click.stop="generateWarningConfig(item)"
+        >
+          <v-icon dark> mdi-upload</v-icon>
+        </v-btn>
+      </UiTooltip>
+    </v-card-actions>
+    <UiWarning />
   </v-card>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 import { faDate } from '~/utils/general'
 export default {
   name: 'ArticleCardComponent',
@@ -18,10 +46,26 @@ export default {
       required: true,
     },
   },
+  computed: {
+    isPublished() {
+      return this.item.isPublished
+    },
+  },
   methods: {
     faDate,
+    ...mapActions('warningGenerator', ['generateWarning']),
+    generateWarningConfig({ isPublished, title }) {
+      const _title = isPublished
+        ? 'خارج کردن پست از حالت انتشار'
+        : 'منتشر کردن پست'
+      const color = isPublished ? 'red' : 'primary'
+      const config = {
+        color,
+        title: _title + ' ' + title,
+        text: 'آیا عملیات مورد تایید است؟',
+      }
+      this.generateWarning(config)
+    },
   },
 }
 </script>
-
-<style></style>
