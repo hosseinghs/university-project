289d80a41e04bc7b2e42da6fcaa01b5eca4d2e5d
Hossein Samani <samanihossein1379@gmail.com>
2022-03-23 21:01:46 +0430

    we have bug

--- server/routes/admin/articles.js
+++ server/routes/admin/articles.js
@@ -22,13 +22,20 @@ router.get("/get", checkToken, async (req, res) => {
   const { type } = req.query;
   const allArticlesType = 2;
   const findAllArticles = "SELECT * FROM article";
-  const findPublishedOrUnPublishedArticles = `SELECT * FROM article where isPublished = ${type}`;
+  const findPublishedOrUnPublishedArticles = `SELECT * FROM article WHERE isPublished = ${type}`;
   const sqlRes =
     +type === allArticlesType
       ? await sql.query(findAllArticles)
       : await sql.query(findPublishedOrUnPublishedArticles);
   const articlesList = sqlRes.recordsets[0];
   res.status(200).send({ success: true, res: articlesList });
+});
+
+router.get("/changePublishmentState", (req, res) => {
+  const { id } = req.query;
+  const resSql = sql.query`SELECT * FROM article WHERE id === ${id}`;
+  console.log(resSql);
+  return res.status(200).send(success);
 });
 
 module.exports = router;
