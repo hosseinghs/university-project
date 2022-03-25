d6009d408c72dc58141e79c8fbfc8b472df6e846
Hossein Samani <samanihossein1379@gmail.com>
2022-03-25 17:02:26 +0430

    register routes added to backend

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+const clientRegisterRoutes = require("./routes/client/register");
+app.use("/client/register", clientRegisterRoutes);
+
 const adminArticlesRoutes = require("./routes/admin/articles");
 app.use("/admin/articles", adminArticlesRoutes);
 
