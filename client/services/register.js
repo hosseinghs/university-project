ea4a3d846a17e658d7ec2ee87e78448a404fdc70
Hossein Samani <samanihossein1379@gmail.com>
2022-03-25 17:05:23 +0430

    baseUrl for register service fixed

--- client/services/register.js
+++ client/services/register.js
@@ -1,9 +1,9 @@
-const baseUrl = 'user';
+const baseUrl = 'register';
 
 export function signUpUserApi(api, newUserData) {
-  return api.$post(`${baseUrl}/register`, newUserData);
+  return api.$post(baseUrl + '/newUser', newUserData);
 }
 
 export function loginApi(api, user) {
-  return api.$post(`${baseUrl}/login`, user);
+  return api.$post(baseUrl + '/login', user);
 }
