0edcf3fd70d8c0e9deaa3590563d7be928134064
Hossein Samani <samanihossein1379@gmail.com>
2022-04-15 13:16:19 +0430

    session used as a middleware in server

--- server/app.js
+++ server/app.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
+const session = require("express-session");
 app.listen(1000);
 require("./utils/db")();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
+app.use(
+  session({
+    secret: "secretKey",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 const clientRegisterRoutes = require("./routes/client/register");
 app.use("/client/register", clientRegisterRoutes);
