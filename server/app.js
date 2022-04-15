79a4b1e87e7360ceb154396d488929535dd26044
Hossein Samani <samanihossein1379@gmail.com>
2022-04-15 14:27:45 +0430

    session codes deleted

--- server/app.js
+++ server/app.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const app = express();
-const session = require("express-session");
 app.listen(1000);
 require("./utils/db")();
 const cors = require("cors");
 app.use(cors());
 app.use(express.json());
-app.use(
-  session({
-    secret: "secretKey",
-    resave: false,
-    saveUninitialized: false,
-  })
-);
 
 const clientRegisterRoutes = require("./routes/client/register");
 app.use("/client/register", clientRegisterRoutes);
