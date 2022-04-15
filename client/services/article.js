eefaab9893884d012777a0e4960e0023f97e5d69
Hossein Samani <samanihossein1379@gmail.com>
2022-04-15 15:22:25 +0430

    getCategoryApi added

--- client/services/article.js
+++ client/services/article.js
@@ -1,6 +1,9 @@
 const baseUrl = 'articles';
 
-
 export function getArticlesApi(api, type) {
-  return api.$get(baseUrl + `/get?type=${type}`)
+  return api.$get(baseUrl + `/get?type=${type}`);
+}
+
+export function getCategoryApi(api) {
+  return api.$get(baseUrl + '/category');
 }
