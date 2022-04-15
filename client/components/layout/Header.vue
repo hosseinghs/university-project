0ac3961cc2ffec62beb002fd6af18af71e911c59
Hossein Samani <samanihossein1379@gmail.com>
2022-04-15 15:31:25 +0430

    hr added and fixed

--- client/components/layout/Header.vue
+++ client/components/layout/Header.vue
@@ -4,6 +4,7 @@
     elevation="1"
     app
   >
+    <hr class="__red__divider" />
     <v-row>
       <v-col>
         <nuxt-link to="/">
@@ -18,3 +19,15 @@
 export default {};
 </script>
 
+<style scoped>
+.__red__divider {
+  position: absolute;
+  width: 100%;
+  height: 4px;
+  padding: 0;
+  background-color: #9e2558;
+  top: 0;
+  left: 0;
+  z-index: 10 !important;
+}
+</style>
