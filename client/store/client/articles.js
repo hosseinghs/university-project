b8283fa589548028bd52d2587ee8b9e3fbb97dfe
Hossein Samani <samanihossein1379@gmail.com>
2022-04-15 15:23:46 +0430

    getCategories action added

--- client/store/client/articles.js
+++ client/store/client/articles.js
@@ -1,14 +1,20 @@
 import { addToArr } from '~/utils/general';
-import { getArticlesApi } from '~/services/article';
+import { getCategoryApi, getArticlesApi } from '~/services/article';
 
 export default {
   namespaced: true,
 
   state: () => ({
+    categories: [],
     articles: [],
   }),
 
-  mutation: {
+  mutations: {
+    SET_CATEGORIES(state, categories) {
+      const list = state.categories;
+      list.splice(0);
+      addToArr(list, categories);
+    },
     SET_ARTICLES(state, arr) {
       const list = state.articles;
       list.splice(0);
@@ -17,6 +23,15 @@ export default {
   },
 
   actions: {
+    async getCategories({ commit }) {
+      async function apiCall(api) {
+        const { res, success } = await getCategoryApi(api);
+        if (success) commit('SET_CATEGORIES', res);
+        return success;
+      }
+      return await this.$apiCaller(apiCall)();
+    },
+
     async getArticles({ commit }, type = 2) {
       async function apiCall(api) {
         const { success, res } = await getArticlesApi(api, type);
