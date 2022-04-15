3a1e8e4461e94309a3b900a96e5a908043e5b412
Hossein Samani <samanihossein1379@gmail.com>
2022-04-15 15:23:58 +0430

    fireApies added to aritcle page

--- client/pages/articles/index.vue
+++ client/pages/articles/index.vue
@@ -3,7 +3,21 @@
 </template>
 
 <script>
-export default {};
+import { mapActions } from 'vuex';
+
+export default {
+  created() {
+    this.fireApies();
+  },
+
+  methods: {
+    ...mapActions('client/articles', ['getArticles', 'getCategories']),
+    fireApies() {
+      const promises = [this.getCategories(), this.getArticles()];
+      Promise.all(promises);
+    },
+  },
+};
 </script>
 
 <style></style>
