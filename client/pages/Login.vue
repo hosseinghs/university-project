42e8a539c0501e10813c4f18839cf21f85b5cbc1
Hossein Samani <samanihossein1379@gmail.com>
2022-04-27 00:00:17 +0430

    ui login fixed

--- client/pages/Login.vue
+++ client/pages/Login.vue
@@ -1,43 +1,45 @@
 <template>
-  <v-container>
-    <v-card class="px-4 py-4 mx-auto">
-      <v-form ref="login" @submit.prevent="submitForm()">
-        <h2>به ادمین خوش آمدید!</h2>
-        <h5>برای ادامه وارد شوید</h5>
-        <v-divider class="mt-2 mb-5"></v-divider>
-        <v-row>
-          <v-col cols="12">
-            <FormText
-              label="شماره تلفن"
-              :rules="[mustFillRule, mobileLengthRule, PhoneNumberRule]"
-              @change="setUserLoginData({ k: 'phoneNumber', v: $event })"
-            />
-          </v-col>
-          <v-col cols="12">
-            <FormText
-              label="رمزعبور"
-              :rules="[mustFillRule]"
-              :type="showPassword ? 'text' : 'password'"
-              :append-icon="showPassword ? icons.hide : icons.show"
-              @click:append="showPassword = !showPassword"
-              @change="setUserLoginData({ k: 'password', v: $event })"
-            />
-          </v-col>
-        </v-row>
-        <v-btn block type="submit" class="primary mt-5">ورود</v-btn>
-      </v-form>
-    </v-card>
-  </v-container>
+  <v-row>
+    <v-col cols="11" md="10" lg="6" xl="4" class="mx-auto">
+      <v-card class="px-4 py-4 mx-auto">
+        <v-form ref="login" @submit.prevent="submitForm()">
+          <h2>به ادمین خوش آمدید!</h2>
+          <h5>برای ادامه وارد شوید</h5>
+          <v-divider class="mt-2 mb-5"></v-divider>
+          <v-row>
+            <v-col cols="12">
+              <FormText
+                label="شماره تلفن"
+                :rules="[mustFillRule, mobileLengthRule, PhoneNumberRule]"
+                @change="setUserLoginData({ k: 'phoneNumber', v: $event })"
+              />
+            </v-col>
+            <v-col cols="12">
+              <FormText
+                label="رمزعبور"
+                :rules="[mustFillRule]"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? icons.hide : icons.show"
+                @click:append="showPassword = !showPassword"
+                @change="setUserLoginData({ k: 'password', v: $event })"
+              />
+            </v-col>
+          </v-row>
+          <v-btn block type="submit" class="primary mt-5">ورود</v-btn>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import { mdiEye, mdiEyeOff, mdiReload } from '@mdi/js'
+import { mapState, mapActions } from 'vuex';
+import { mdiEye, mdiEyeOff, mdiReload } from '@mdi/js';
 import {
   mustFillRule,
   mobileLengthRule,
   PhoneNumberRule,
-} from '~/utils/validations'
+} from '~/utils/validations';
 
 export default {
   name: 'LoginPage',
@@ -50,7 +52,7 @@ export default {
         reload: mdiReload,
       },
       showPassword: false,
-    }
+    };
   },
   computed: {
     ...mapState('register', ['isLoggedIn']),
@@ -62,10 +64,10 @@ export default {
     ...mapActions('register', ['loginUser', 'setUserLoginData']),
     async submitForm() {
       if (this.$refs.login.validate()) {
-        const res = await this.loginUser()
-        if (res) this.$router.push({ path: '/articles' })
+        const res = await this.loginUser();
+        if (res) this.$router.push({ path: '/articles' });
       }
     },
   },
-}
+};
 </script>
