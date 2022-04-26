67b3a1368c468105f735b4a7599616d5b0772741
Hossein Samani <samanihossein1379@gmail.com>
2022-04-27 00:59:28 +0430

    article page added

--- client/pages/articles/_id.vue
+++ client/pages/articles/_id.vue
@@ -1,5 +1,19 @@
 <template>
-  <div></div>
+  <v-container>
+    <v-row>
+      <v-col cols="12" lg="8">
+        <v-card class="rounded-lg" elevation="2">
+          <v-card-title class="title">{{ article.title }}</v-card-title>
+          <v-card-text>
+            <p>
+              <span class="author">{{ article.author }}</span>
+            </p>
+          </v-card-text>
+          <v-card-text v-html="article.text"></v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -12,14 +26,26 @@ export default {
   },
 
   created() {
-      const articleId = this.$route.params.id
-      articleId && this.getArticleById(articleId)
+    const articleId = this.$route.params.id;
+    articleId && this.getArticleById(articleId);
   },
-  
+
   methods: {
     ...mapActions('client/articles', ['getArticleById']),
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+.title {
+  color: #444444;
+  line-height: 1.3;
+  font-size: 30px;
+  font-weight: 700;
+}
+.author {
+  margin-left: auto;
+  font-weight: 700;
+  color: #4d4d4d;
+}
+</style>
