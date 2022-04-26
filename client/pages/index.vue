1f1aa38e95ebd4a44bb4d4737be5df660dbfbf43
Hossein Samani <samanihossein1379@gmail.com>
2022-04-27 00:01:45 +0430

    ui fixed

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,57 +1,59 @@
 <template>
-  <v-container>
-    <v-card class="px-4 py-4 mx-auto" max-width="700">
-      <v-form ref="login" @submit.prevent="submitForm()">
-        <h2>خوش آمدید!</h2>
-        <h5>ثبت نام کنید!</h5>
-        <v-divider class="mt-2 mb-5"></v-divider>
-        <v-row>
-          <v-col cols="12">
-            <FormText
-              label="نام"
-              :rules="[mustFillRule]"
-              @change="setUserRegisterData({ k: 'firstName', v: $event })"
-            />
-          </v-col>
-          <v-col cols="12">
-            <FormText
-              label="نام خانوادگی"
-              :rules="[mustFillRule]"
-              @change="setUserRegisterData({ k: 'lastName', v: $event })"
-            />
-          </v-col>
-          <v-col cols="12">
-            <FormText
-              label="ایمیل"
-              :rules="[mustFillRule, emailFormatRule]"
-              @change="setUserRegisterData({ k: 'email', v: $event })"
-            />
-          </v-col>
-          <v-col cols="12">
-            <FormText
-              label="شماره تلفن"
-              :rules="[mustFillRule, mobileLengthRule, PhoneNumberRule]"
-              @change="setUserRegisterData({ k: 'phoneNumber', v: $event })"
-            />
-          </v-col>
-          <v-col cols="12">
-            <FormText label="تاریخ تولد" />
-          </v-col>
-          <v-col cols="12">
-            <FormText
-              label="رمزعبور"
-              :rules="[mustFillRule]"
-              :type="showPassword ? 'text' : 'password'"
-              :append-icon="showPassword ? icons.hide : icons.show"
-              @click:append="showPassword = !showPassword"
-              @change="setUserRegisterData({ k: 'password', v: $event })"
-            />
-          </v-col>
-        </v-row>
-        <v-btn block type="submit" class="primary mt-5">ورود</v-btn>
-      </v-form>
-    </v-card>
-  </v-container>
+  <v-row>
+    <v-col cols="11" md="10" lg="6" xl="4" class="mx-auto">
+      <v-card class="px-4 py-4 mx-auto" max-width="700">
+        <v-form ref="login" @submit.prevent="submitForm()">
+          <h2>خوش آمدید!</h2>
+          <h5>ثبت نام کنید!</h5>
+          <v-divider class="mt-2 mb-5"></v-divider>
+          <v-row>
+            <v-col cols="12">
+              <FormText
+                label="نام"
+                :rules="[mustFillRule]"
+                @change="setUserRegisterData({ k: 'firstName', v: $event })"
+              />
+            </v-col>
+            <v-col cols="12">
+              <FormText
+                label="نام خانوادگی"
+                :rules="[mustFillRule]"
+                @change="setUserRegisterData({ k: 'lastName', v: $event })"
+              />
+            </v-col>
+            <v-col cols="12">
+              <FormText
+                label="ایمیل"
+                :rules="[mustFillRule, emailFormatRule]"
+                @change="setUserRegisterData({ k: 'email', v: $event })"
+              />
+            </v-col>
+            <v-col cols="12">
+              <FormText
+                label="شماره تلفن"
+                :rules="[mustFillRule, mobileLengthRule, PhoneNumberRule]"
+                @change="setUserRegisterData({ k: 'phoneNumber', v: $event })"
+              />
+            </v-col>
+            <v-col cols="12">
+              <FormText label="تاریخ تولد" />
+            </v-col>
+            <v-col cols="12">
+              <FormText
+                label="رمزعبور"
+                :rules="[mustFillRule]"
+                :type="showPassword ? 'text' : 'password'"
+                :append-icon="showPassword ? icons.hide : icons.show"
+                @click:append="showPassword = !showPassword"
+                @change="setUserRegisterData({ k: 'password', v: $event })"
+              />
+            </v-col>
+          </v-row>
+          <v-btn block type="submit" class="primary mt-5">ورود</v-btn>
+        </v-form>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
