ac78eb72831ca5e88bae7d8aae033fe40c40b9dd
Hossein Samani <samanihossein1379@gmail.com>
2022-04-27 00:28:03 +0430

    getArticleById bined to single article page

---
+++ client/pages/articles/_id.vue
@@ -0,0 +1,25 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+export default {
+  name: 'SingleArticlePage',
+
+  computed: {
+    ...mapState('client/articles', ['article']),
+  },
+
+  created() {
+      const articleId = this.$route.params.articleId
+      articleId && this.getArticleById(articleId)
+  },
+  
+  methods: {
+    ...mapActions('client/articles', ['getArticleById']),
+  },
+};
+</script>
+
+<style></style>
