b49762da2188a7b727cb345c1a4ecf0811c8fca0
Hossein Samani <samanihossein1379@gmail.com>
2022-05-10 21:12:51 +0430

    quill editor installed on project

---
+++ admin/components/form/Editor.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="quillEditor">
+    <quillEditor
+      v-model="content"
+      class="quill-editor"
+      @change="$emit('onEditorChange', $event)"
+    ></quillEditor>
+  </div>
+</template>
+
+<script>
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
+import { quillEditor } from 'vue-quill-editor'
+
+export default {
+  name: 'EditorComponent',
+  components: {
+    quillEditor,
+  },
+  data() {
+    return {
+      content: '',
+    }
+  },
+}
+</script>
+
+<style>
+#quillEditor .ql-editor {
+  text-align: right !important;
+  direction: rtl !important;
+}
+
+#quillEditor .ql-container {
+  background-color: #fff !important;
+}
+</style>
