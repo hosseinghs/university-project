3c02f039f68988739026d369f0581c0b6773fb5e
Hossein Samani <samanihossein1379@gmail.com>
2022-08-25 01:46:32 +0430

    editArticleApi added

--- admin/services/articles.js
+++ admin/services/articles.js
@@ -4,6 +4,10 @@ export function createArticleApi(api, article) {
   return api.$post(baseUrl + '/create', article)
 }
 
+export function editArticleApi(api, article) {
+  return api.$put(baseUrl + '/create', article)
+}
+
 export function getCategoryApi(api) {
   return api.$get(baseUrl + '/category')
 }
